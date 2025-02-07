b8f57ea684d1a708d2b7a7b20f309de55b05d9fc
Naveen <naveenkulaljs2@gmail.com>
2025-02-07 14:34:09 +0530

    removed app

--- app.js
+++
@@ -1,9 +0,0 @@
-async function display() {
-    let fact = await require("./fact.js");
-    console.log("Fact is ",fact)
-}
-
-display();
-
-let fact = require("./fact");
-console.log("fact is",fact);
--- fact.js
+++ fact.js
@@ -1,20 +1,3 @@
-// async function getFact() {
-//     console.log("Before fetch");
-//     return await fetch("https://catfact.ninja/fact");
-// }
-
-// console.log("Before get");
-// async function displayFact(){
-//     await getFact().then((res)=>{
-//         return res.json();
-//     }).then((data)=>{
-//         //console.log(data.fact);
-//         return data.fact;
-//     })
-// }
-// console.log("After get");
-
-// console.log(displayFact());
 let url = "https://catfact.ninja/fact";
 
 async function getFact(){
